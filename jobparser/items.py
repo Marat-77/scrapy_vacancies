37275d4f1e35f7cfb7955fed3d2422cd8417dacc
zmd <maratz375@gmail.com>
2022-07-22 16:24:50 +0500

    scrapy_vacancies
    items.py

--- jobparser/items.py
+++ jobparser/items.py
@@ -8,5 +8,16 @@ import scrapy
 
 class JobparserItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    _id = scrapy.Field()
+    vacancy_title = scrapy.Field()
+    vacancy_url = scrapy.Field()
+    min_salary = scrapy.Field()
+    max_salary = scrapy.Field()
+    cur_salary = scrapy.Field()
+    vacancy_company_name = scrapy.Field()
+    vacancy_company_link = scrapy.Field()
+    vacancy_require_experience = scrapy.Field()
+    vacancy_employment_mode = scrapy.Field()
+    vacancy_tag_skills = scrapy.Field()
+    vacancy_description = scrapy.Field()
+    vacancy_creation_time = scrapy.Field()
